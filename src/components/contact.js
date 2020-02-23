6c11d7053584f3de4151ee7c7cd5390773726b76
Igor Apolinário <igor_apolinario96@hotmail.com>
2020-02-23 12:42:03 -0300

    Adicionado novo formulário para teste

--- src/components/contact.js
+++ src/components/contact.js
@@ -22,29 +22,26 @@ export default class IndexPage extends React.Component {
 
   render() {
     return (
-      <form name="contact" netlify>
-        <label>
-          Name
-          <input type="text" name="name" id="name" />
-        </label>
-
-        <label>
-          Email
-          <input type="email" name="email" id="email" />
-        </label>
-
-        <label>
-          Subject
-          <input type="text" name="subject" id="subject" />
-        </label>
-
-        <label>
-          Message
-          <textarea name="message" id="message" rows="5" />
-        </label>
-
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
         <button type="submit">Send</button>
-      </form>
+      </p>
+    </form>
     )
   }
 }
